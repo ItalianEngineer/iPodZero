0670ddbfd391f15dbde9672e3d55299ba9720b4a
John-Develops <98222196+John-Develops@users.noreply.github.com>
2023-08-19 13:20:17 -0400

    Create click.c
    This is a rework of Guy Dupont's click.c code, wiring the button positions and wheel positions to keyboard buttons.

---
+++ click.c
@@ -0,0 +1,190 @@
+// To compile on the pi (after installing pigpio):
+// gcc -Wall -pthread -o click click.c -lpigpio -lrt
+
+#include <pigpio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define CLOCK_PIN 23
+#define DATA_PIN 25
+#define HAPTIC_PIN 26
+#define BIT_COUNT 32
+#define PORT 9090 
+#define MAXLINE 1024 
+
+///////////////////////////////////////////////////////////////////////////////////
+
+#define CENTER_BUTTON_BIT 7
+#define LEFT_BUTTON_BIT 9
+#define RIGHT_BUTTON_BIT 8
+#define UP_BUTTON_BIT 11
+#define DOWN_BUTTON_BIT 10
+#define WHEEL_TOUCH_BIT 29
+
+///////////////////////////////////////////////////////////////////////////////////
+
+#define BUFFER_SIZE 3
+#define BUTTON_INDEX 0
+#define BUTTON_STATE_INDEX 1
+#define WHEEL_POSITION_INDEX 2
+
+// used to store the current packet
+uint32_t bits = 0;
+// used to store the previous full packet
+uint32_t lastBits = 0;
+uint8_t bitIndex = 0;
+uint8_t oneCount = 0;
+uint8_t recording = 0;
+// indicates whether the data pin is high or low
+uint8_t dataBit = 1;
+uint8_t lastPosition = 255;
+int hapticWaveId = -1;
+
+char buttons[] = { 
+    CENTER_BUTTON_BIT, 
+    LEFT_BUTTON_BIT, 
+    RIGHT_BUTTON_BIT, 
+    UP_BUTTON_BIT, 
+    DOWN_BUTTON_BIT, 
+    WHEEL_TOUCH_BIT
+};
+
+// all valid click wheel packets start with this
+const uint32_t PACKET_START = 0b01101;
+
+int sockfd; 
+char buffer[BUFFER_SIZE]; 
+char prev_buffer[BUFFER_SIZE];
+struct sockaddr_in servaddr; 
+
+// helper function to print packets as binary
+void printBinary(uint32_t value) {
+    for(uint8_t i = 0; i < 32; i++) {
+        if (value & 1)
+            printf("1");
+        else
+            printf("0");
+
+        value >>= 1;
+    }
+    printf("\n");
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// parse packet and broadcast data
+void sendPacket() {
+    // ... (existing code)
+    
+    uint8_t wheelPosition = (bits >> 16) & 0xFF;
+
+    // Translate wheel rotation to ncmpcpp commands
+    if (wheelPosition != lastPosition) {
+        if (wheelPosition > lastPosition) {
+            // Rotate clockwise (e.g., "next track" command)
+            system("ncmpcpp next");
+        } else {
+            // Rotate counter-clockwise (e.g., "previous track" command)
+            system("ncmpcpp prev");
+        }
+        lastPosition = wheelPosition;
+    }
+
+    // Translate button presses to ncmpcpp commands
+    if (buffer[BUTTON_INDEX] == CENTER_BUTTON_BIT && buffer[BUTTON_STATE_INDEX] == 1) {
+        system("ncmpcpp toggle");
+    } else if (buffer[BUTTON_INDEX] == LEFT_BUTTON_BIT && buffer[BUTTON_STATE_INDEX] == 1) {
+        system("ncmpcpp prev");
+    } else if (buffer[BUTTON_INDEX] == RIGHT_BUTTON_BIT && buffer[BUTTON_STATE_INDEX] == 1) {
+        system("ncmpcpp next");
+    } else if (buffer[BUTTON_INDEX] == UP_BUTTON_BIT && buffer[BUTTON_STATE_INDEX] == 1) {
+        system("ncmpcpp prev");
+    } else if (buffer[BUTTON_INDEX] == DOWN_BUTTON_BIT && buffer[BUTTON_STATE_INDEX] == 1) {
+        system("ncmpcpp next");
+    }
+///////////////////////////////////////////////////////////////////////////////////
+
+void onClockEdge(int gpio, int level, uint32_t tick) {
+    if (!level) {
+        // only care about rising edge
+        return;
+    }
+    if (dataBit == 0) {
+        recording = 1;
+        oneCount = 0;
+    } else {
+        // 32 1's in a row means we're definitely not in the middle of a packet
+        if (++oneCount >= BIT_COUNT) {
+            recording = 0;
+            bitIndex = 0;
+        }
+    }
+    // in the middle of the packet
+    if (recording == 1) {
+        if (dataBit) {
+            bits = setBit(bits, bitIndex);
+        } else {
+            bits = clearBit(bits, bitIndex);
+        }
+        // we've collected the whole packet
+        if (++bitIndex == 32) {
+            bitIndex = 0;
+            sendPacket();
+        }
+    }
+}
+
+void onDataEdge(int gpio, int level, uint32_t tick) {
+    dataBit = level;
+}
+
+int main(void *args){
+  
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+  
+    memset(&servaddr, 0, sizeof(servaddr)); 
+      
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+
+    if (gpioInitialise() < 0) {
+       exit(1);
+    }
+
+    // haptic waveform - just a simple on-off pulse
+    gpioSetMode(HAPTIC_PIN, PI_OUTPUT);
+    gpioPulse_t pulse[2];
+    pulse[0].gpioOn = (1<<HAPTIC_PIN);
+    pulse[0].gpioOff = 0;
+    pulse[0].usDelay = 8000;
+
+    pulse[1].gpioOn = 0;
+    pulse[1].gpioOff = (1<<HAPTIC_PIN);
+    pulse[1].usDelay = 2000;
+
+    gpioWaveAddNew();
+
+    gpioWaveAddGeneric(2, pulse);
+
+    hapticWaveId = gpioWaveCreate();
+    gpioSetPullUpDown(CLOCK_PIN, PI_PUD_UP);
+    gpioSetPullUpDown(DATA_PIN, PI_PUD_UP);
+    gpioSetAlertFunc(CLOCK_PIN, onClockEdge);
+    gpioSetAlertFunc(DATA_PIN, onDataEdge);
+
+    while(1) {
+
+    };
+    gpioTerminate();
+}
