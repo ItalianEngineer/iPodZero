178086550f7c05421060afcfeb8074b042df5db5
John-Develops <98222196+John-Develops@users.noreply.github.com>
2023-08-20 12:14:58 -0400

    Update Clickwheel.py
    This code mainly detects down movement more than up, so I am working on getting those detections evened out.

--- Clickwheel.py
+++ Clickwheel.py
@@ -12,6 +12,7 @@ GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Set up the variables to keep track of the click wheel state
 clkLastState = GPIO.input(CLK)
+rotation_count = 0
 
 while True:
     clkState = GPIO.input(CLK)
@@ -19,12 +20,17 @@ while True:
 
     if clkState != clkLastState:
         if dtState != clkState:
-            # clockwise rotation
-            print("UP key activated")
-            time.sleep(0.1)
+            # Clockwise rotation
+            rotation_count += 1
         else:
-            # counter-clockwise rotation
+            # Counter-clockwise rotation
+            rotation_count -= 1
+
+        if rotation_count >= 3:
+            print("UP key activated")
+            rotation_count = 0
+        elif rotation_count <= -3:
             print("DOWN key activated")
-            time.sleep(0.1)
+            rotation_count = 0
 
     clkLastState = clkState
